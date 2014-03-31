e7d815da0c8bc18ea140e90a1b3d2d0f19191b53
saley Maiga <saleymaiga@gmail.com>
2014-03-30 20:47:18 -0400

    topic form added

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,20 +1,29 @@
 class PostsController < ApplicationController
   def index
+  	@user =User.find(params[:user_id])
+  	@posts = Post.where(user_id: params[:user_id])
   end
 
   def new
-  	@post = Post.new
-  	@topic = Topic.new
+  	@topic = Topic.new(params[:topic])
   	
+
   end
 
   def create
-  	@post = Post.new(params[:post])
   	@topic = Topic.new(params[:topic])
+  	if @topic.save
+  		flash[:notice] = "Your post was created successfully"
+  		redirect_to root_path
+  	else
+  		flash[:notice]= "There was a problem creating that post"
+  		redirect_to user_path
+  	end
   	
   end
 
   def show
+  	@post = Post.find(params[:id])
   end
 
   def edit
